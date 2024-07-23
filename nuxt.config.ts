ff8591b7ebe05c7871e75cb0778463e63a09d0af
Istratov Alexey <istratov_alexey@bk.ru>
2024-07-24 03:05:22 +0400

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 export default defineNuxtConfig({
   app: {
+    baseURL: '/nuxt-tasks/',
+    buildAssetsDir: 'assets',
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
